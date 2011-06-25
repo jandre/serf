c3b29015d43917c6b12c37f5890ce886d40f10c2
justin.erenkrantz <justin.erenkrantz@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2011-06-25 16:01:01 +0000

    Send TLS SNI info when available.
    * serf_bucket_types.h
      (serf_ssl_set_hostname): Add function to tell OpenSSL bucket what host we
      think we're connecting to.
    * buckets/ssl_buckets.c
      (serf_ssl_set_hostname): Set SNI tlsext if our OpenSSL supports it.
    * test/serf_get.c
      (app_baton_t): Add hostinfo.
      (conn_setup): Pass hostinfo to SSL bucket.
      (main): Stash hostinfo for later use.

--- trunk/buckets/ssl_buckets.c
+++ trunk/buckets/ssl_buckets.c
@@ -1066,7 +1066,15 @@ static serf_bucket_t * serf_bucket_ssl_create(
     return serf_bucket_create(type, allocator, ctx);
 }
 
-SERF_DECLARE(serf_bucket_t *) serfapr_status_t serf_ssl_use_default_certificates(serf_ssl_context_t *ssl_ctx)
+SERF_DECLARE(serf_bucket_t *) serfapr_status_t serf_ssl_set_hostname(serf_ssl_context_t *context,
+                                   const char * hostname)
+{
+#ifdef SSL_set_tlsext_host_name
+    if (SSL_set_tlsext_host_name(context->ssl, hostname) != 1) {
+        ERR_clear_error();
+    }
+#endif
+    return APR_SUCCESS serfapr_status_t serf_ssl_use_default_certificates(serf_ssl_context_t *ssl_ctx)
 {
     X509_STORE *store = SSL_CTX_get_cert_store(ssl_ctx->ctx);
 
