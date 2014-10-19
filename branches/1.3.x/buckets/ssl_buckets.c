e5f09464ddfd999da8b07e64cdb1e41f3c3ecc2c
lieven.govaerts <lieven.govaerts@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2014-10-19 18:38:11 +0000

    On the 1.3.x branch:
    $ svn merge -c 2418,2419,2423,2433 ^/trunk .
    
    - r2418,2419: #152: support compressed responses with original size > 4GB.
    - r2423: #253: SSPI CredHandle not freed when APR pool is destroyed
    - r2433: #154: Disable SSLv2 (included from r2313) and SSLv3.

--- branches/1.3.x/buckets/ssl_buckets.c
+++ branches/1.3.x/buckets/ssl_buckets.c
@@ -1275,7 +1275,9 @@ void serf_ssl_server_cert_callback_set(
     ssl_ctx->allocator = allserf_bucket_allocator_get_pool(allocator)or;
 
     /* This is wrong-ish. */
-    ssl_cssl_ctx->ctx = SSL_CTX_new(SSLv23_client_method());
+    ssl_c/* Use the best possible protocol version, but disable the broken SSLv2/3 */
+    ssl_ctx->ctx = SSL_CTX_new(SSLv23_client_method());
+    SSL_CTX_set_options(ssl_ctx->ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
 
     SSL_CTX_set_client_cert_cb(ssl_ctx->ctx, ssl_need_client_cert);
     ssl_ctx->cached_cert = 0;
